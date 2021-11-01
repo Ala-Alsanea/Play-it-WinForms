4e11bdc05da144cc75654ac4dd762361c3654ca3
Ala-alsanea <ala0alsanea@gmail.com>
2021-11-01 11:04:05 +0300

    initial

---
+++ Play it [winForm]/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Play_it__winForm_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_start = new System.Windows.Forms.Label();
+            this.lbl_inProgress = new System.Windows.Forms.Label();
+            this.guna2TrackBar1 = new Guna.UI2.WinForms.Guna2TrackBar();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_opebFile = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.guna2TrackBar2 = new Guna.UI2.WinForms.Guna2TrackBar();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.guna2TrackBar2);
+            this.panel1.Controls.Add(this.btn_opebFile);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btn_stop);
+            this.panel1.Controls.Add(this.btn_start);
+            this.panel1.Controls.Add(this.guna2TrackBar1);
+            this.panel1.Controls.Add(this.lbl_inProgress);
+            this.panel1.Controls.Add(this.lbl_start);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 402);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(963, 94);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbl_start
+            // 
+            this.lbl_start.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_start.AutoSize = true;
+            this.lbl_start.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_start.Location = new System.Drawing.Point(12, 16);
+            this.lbl_start.Name = "lbl_start";
+            this.lbl_start.Size = new System.Drawing.Size(39, 17);
+            this.lbl_start.TabIndex = 1;
+            this.lbl_start.Text = "00:00";
+            // 
+            // lbl_inProgress
+            // 
+            this.lbl_inProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_inProgress.AutoSize = true;
+            this.lbl_inProgress.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_inProgress.Location = new System.Drawing.Point(912, 16);
+            this.lbl_inProgress.Name = "lbl_inProgress";
+            this.lbl_inProgress.Size = new System.Drawing.Size(39, 17);
+            this.lbl_inProgress.TabIndex = 1;
+            this.lbl_inProgress.Text = "00:00";
+            // 
+            // guna2TrackBar1
+            // 
+            this.guna2TrackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2TrackBar1.FillColor = System.Drawing.Color.White;
+            this.guna2TrackBar1.HoverState.Parent = this.guna2TrackBar1;
+            this.guna2TrackBar1.Location = new System.Drawing.Point(57, 16);
+            this.guna2TrackBar1.Name = "guna2TrackBar1";
+            this.guna2TrackBar1.Size = new System.Drawing.Size(849, 23);
+            this.guna2TrackBar1.TabIndex = 2;
+            this.guna2TrackBar1.ThumbColor = System.Drawing.Color.Black;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(441, 59);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(522, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "post";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "pre";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(441, 59);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 3;
+            this.btn_stop.Text = "stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            // 
+            // btn_opebFile
+            // 
+            this.btn_opebFile.Location = new System.Drawing.Point(15, 59);
+            this.btn_opebFile.Name = "btn_opebFile";
+            this.btn_opebFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_opebFile.TabIndex = 3;
+            this.btn_opebFile.Text = "open file";
+            this.btn_opebFile.UseVisualStyleBackColor = true;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(963, 402);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            // 
+            // guna2TrackBar2
+            // 
+            this.guna2TrackBar2.FillColor = System.Drawing.Color.White;
+            this.guna2TrackBar2.HoverState.Parent = this.guna2TrackBar2;
+            this.guna2TrackBar2.Location = new System.Drawing.Point(751, 59);
+            this.guna2TrackBar2.Name = "guna2TrackBar2";
+            this.guna2TrackBar2.Size = new System.Drawing.Size(166, 23);
+            this.guna2TrackBar2.TabIndex = 4;
+            this.guna2TrackBar2.ThumbColor = System.Drawing.Color.Black;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 496);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2TrackBar guna2TrackBar1;
+        private System.Windows.Forms.Label lbl_inProgress;
+        private System.Windows.Forms.Label lbl_start;
+        private System.Windows.Forms.Button btn_opebFile;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_start;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private Guna.UI2.WinForms.Guna2TrackBar guna2TrackBar2;
+    }
+}
+
